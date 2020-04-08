fda4fc35559767b4060517cf51246a66246b2e0f
zhiyuzhang <pmozhang@gmail.com>
2020-04-09 03:45:09 +0800

    Fixed bugs, and add interpolations

--- critical_density.py
+++ critical_density.py
@@ -21,18 +21,25 @@ Written by Zhi-Yu Zhang
 pmozhang@gmail.com 
 
 Last update: 
-    23 Jan. 2018 
+    09 Apr. 2020 
+
+updated the bugs in calculation the upper-word coefficient. Now it gives similar results as Shirley's results
+updated interpolation for any given temperature
 
 
 Next to do: 
 
-    Interpolate Cij using values from nearby temperatures
-    Currently, only the temperature value from the datafile can be used
+1. Be more automatic in reading collision files. Now we are still specifying each of them.
+
+2. recognise ortho- and para- parameters automatically.
+
+3. try more complicated molecular transitions, not be limited to rotational transitions.
 
 '''
 
 
 import numpy as np
+from scipy.interpolate import interp1d
 from astropy import constants as const
 
 h=const.h.cgs.value   # 6.626075E-27          #[erg/s]
@@ -75,6 +82,16 @@ def input(molecule, ):
         hd_row_beg  = 36 
         hd_row_read = 25 
         filename    = 'hcn.dat'
+    elif molecule == 'HCO+': 
+        T_row_skip  = 80
+        Cp_row_beg  = 82 
+        Cp_row_read = 465 
+        Co_row_beg  = 82 
+        Co_row_read = 465 
+        hd_row_beg  = 41 
+        hd_row_read = 30 
+        filename    = 'hco+.dat'
+
     else:
         print("input error")
 
@@ -91,6 +108,7 @@ def input(molecule, ):
     A      = header[:,3]
     freq   = header[:,4] * 1E9 # Hz
     Eu     = header[:,5]
+    Eu=np.insert(Eu,0,0)
     return T_list, Cp, Co, A, freq, Eu 
 
 
@@ -100,42 +118,47 @@ def g(j):
     return g
 
 
-# Cr = > collisional rate 
-def Cr(J_low, T, T_list, Co, Cp, ortho_para): 
-    if ortho_para == 'p':
-        i    = np.where(Cp[:,2]     == J_low+1)[0]
-        k    = np.where(T_list      == T      )[0]+3  
-        cij  = Cp[i,k]
-        ju   = Cp[i,1]
-    elif ortho_para == 'o':
-        i    = np.where(Co[:,2]     == J_low+1)[0]
-        k    = np.where(T_list      == T      )[0]+3  
-        cij  = Co[i,k]
-        ju   = Co[i,1]
-    else:
-        print("error in ortho para setup")
-        return 
+def Cij(J_low, J_up, T, T_list, Cr): 
+    i    = np.where( (Cr[:,2]  == J_low+1) & (Cr[:,1] == J_up+1))[0]
+    x    = T_list
+    y    = Cr[i,3:] 
+    f    = interp1d(x, y)
+    cij  = f(T)
     return cij
 
 
-def gamma(J_low,T, T_list, Co, Cp, freq, op): 
-    gamma_ij= g(J_low+1) / g(J_low)  * Cr(J_low,T, T_list,Co,Cp,op) * np.exp( -1. * h * freq[J_low] / (k * T))
+
+
+def gamma(J_low, J_up,T, T_list, Cr, Eu): 
+    cij     = Cij(J_low, J_up, T, T_list, Cr) 
+    Eu_up = Eu[J_up]
+    Eu_low= Eu[J_low]
+    gamma_ij= g(J_up) / g(J_low)  * cij * np.exp( -1. * (Eu_up - Eu_low) /  T)
     return gamma_ij 
 
+def Sigma_gamma(J_low,T, T_list, Cr, Eu):
+    S_gamma=[]
+    # all transitions that go upwards by collisions. 
+    for J_up in  np.arange(J_low+2, 24):
+        S_gamma.append(gamma(J_low+1,J_up,T,T_list,Cr,Eu))
+    Sigma_gamma=np.sum(S_gamma)
+#   print(S_gamma)
+    return Sigma_gamma
 
-def Sigma_gamma_pre(J_low,T, T_list, Co, Cp, freq, op):
-        i     = np.where(Cp[:,1]     == J_low+1)[0]
-        k     = np.where(T_list      == T      )[0]+3  
-        cij   = Cp[i,k]
-        S     = np.sum(cij)
-        return S
+def Sigma_gamma_pre(J_low,T, T_list, Cr, Eu,):
+    i     = np.where(Cr[:,1]     == J_low+2)[0]
+    # all transitions that go downside by collisions. 
+    k     = np.where(T_list      == T      )[0]+3  
+    cij   = Cr[i,k]
+    S   = np.sum(cij)
+    print(S)
+    return S
 
 
 def ncrit(molecule, J_low, T, op_ratio, verbose): 
     T_list, Cp, Co, A, freq, Eu = input(molecule) 
-    ncrit_o = A[J_low] / (np.sum(gamma(J_low,T,T_list,Co,Cp,freq,'o')) + Sigma_gamma_pre(J_low,T,T_list,Co,Cp,freq,'o'))
-    ncrit_p = A[J_low] / (np.sum(gamma(J_low,T,T_list,Co,Cp,freq,'p')) + Sigma_gamma_pre(J_low,T,T_list,Co,Cp,freq,'p'))
-
+    ncrit_o = A[J_low] / (Sigma_gamma(J_low,T,T_list,Co,Eu,) + Sigma_gamma_pre(J_low,T,T_list,Co,Eu,))
+    ncrit_p = A[J_low] / (Sigma_gamma(J_low,T,T_list,Cp,Eu,) + Sigma_gamma_pre(J_low,T,T_list,Cp,Eu,))
     ncrit   = (op_ratio * ncrit_o + 1 * ncrit_p )/(op_ratio+1)  
     trans     = "J="+str(J_low+1)+"-"+str(J_low)
     transition = trans.replace(" ", "")
